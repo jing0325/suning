ac0c984f3f23155573bc3a40c0db06464bf6f9cc
jing0325 <750858196@qq.com>
2020-12-14 23:23:49 +0800

    提交首页代码

--- php/suningjson.php
+++ php/suningjson.php
@@ -11,3 +11,15 @@ if($conn->connect_error){
 }
 
 $conn->query('SET NAMES UTF8');
+
+
+//2.输出接口
+$result=$conn->query("select * from indexdata");
+
+$arr = array();
+
+for($i=0;$i<$result->num_rows;$i++){
+    $arr[$i] = $result->fetch_assoc();
+}
+
+echo json_encode($arr);
